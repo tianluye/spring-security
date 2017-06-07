32a6a22b422b00ab8d89088872367acbf716631e
田陆野 <tian.luye@zte.com.cn>
2017-06-07 10:06:07 +0800

    update code

--- src/main/java/com/tian/spring/web/HelloController.java
+++ src/main/java/com/tian/spring/web/HelloController.java
@@ -3,6 +3,7 @@ package com.tian.spring.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
+    // 如果URL = /welcome 或  / , 返回 hello 页面
     @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
     public ModelAndView welcomePage() {
         ModelAndView model = new ModelAndView();
@@ -20,12 +22,28 @@ public class HelloController {
         return model;
     }
 
+    // 如果 URL = /admin , 返回 admin 页面
     @RequestMapping(value = "/admin**", method = RequestMethod.GET)
     public ModelAndView adminPage() {
         ModelAndView model = new ModelAndView();
         model.addObject("title", "Spring Security Hello World");
         model.addObject("message", "This is protected page!");
         model.setViewName("admin");
+        return model;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+        @RequestParam(value = "error", required = false) String error,
+        @RequestParam(value = "logout", required = false) String logout) {
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
         return model;
     }
 
--- src/main/webapp/WEB-INF/pages/admin.jsp
+++ src/main/webapp/WEB-INF/pages/admin.jsp
@@ -2,12 +2,30 @@
 <%@page session="true" contentType="text/html; charset=UTF-8"%>
 <html>
 <body>
-<h1>标题: ${title}</h1>
-<h1>消息 : ${message}</h1>
+<h1>Title : ${title}</h1>
+<h1>Message : ${message}</h1>
+
+<c:url value="/j_spring_security_logout" var="logoutUrl" />
+
+<!-- csrt for log out-->
+    <form action="${logoutUrl}" method="post" id="logoutForm">
+    <input type="hidden"
+           name="${_csrf.parameterName}"
+           value="${_csrf.token}" />
+</form>
+
+<script>
+    function formSubmit() {
+        document.getElementById("logoutForm").submit();
+    }
+</script>
 
 <c:if test="${pageContext.request.userPrincipal.name != null}">
-    <h2>欢迎: ${pageContext.request.userPrincipal.name}
-        | <a href="<c:url value="/j_spring_security_logout" />" > Logout</a></h2>
+    <h2>
+        Welcome : ${pageContext.request.userPrincipal.name} | <a
+            href="javascript:formSubmit()"> Logout</a>
+    </h2>
 </c:if>
+
 </body>
 </html>
